544b428d7aff6d23fbe4b20fb331e2d8e70d0034
Jonathan Pereira <jonathan.pereira@gmail.com>
2016-01-10 21:49:15 -0200

    Adicionado Tela de UI e insercao de sons no HPP

--- aegis/functions.hpp
+++ aegis/functions.hpp
@@ -15,3 +15,45 @@ class CfgFunctions
 		}
 	};
 };
+
+
+class cfgNotifications {
+  class CivilDeath {
+  title = "%1";
+  iconPicture = "\a3\Ui_F_Curator\Data\CfgMarkers\kia_ca.paa";
+  description = "%2";
+  duration = 10;
+  };
+
+	class Transaction {
+  title = "%1";
+  iconPicture = "aegis\ui\pagamento.paa";
+  description = "%2";
+  duration = 10;
+  };
+
+	class CheckBalance {
+  title = "%1";
+  iconPicture = "aegis\ui\consulta.paa";
+  description = "%2";
+  duration = 10;
+  };
+};
+
+
+class CfgSounds
+{
+	sounds[] = {};
+	class alert
+	{
+		name = "alert";
+		sound[] = {"aegis\sounds\alert.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cash
+	{
+		name = "cash";
+		sound[] = {"aegis\sounds\cash.ogg", 1, 1};
+		titles[] = {};
+	};
+};
